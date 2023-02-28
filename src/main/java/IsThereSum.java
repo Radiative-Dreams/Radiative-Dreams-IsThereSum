c6c89f89decef8e56963419275aebe3b415a3de0
Rey V <radiativedreams@gmail.com>
2023-02-28 06:37:15 +0000

    Completed Coding Challenge Is There Sum

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,12 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        // could have used key-value pairs, but let it be simple for now
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 1; j < arr.length; j++){
+                if(i + j  == target) return true;
+            }
+        }
         return false;
     }
 }
